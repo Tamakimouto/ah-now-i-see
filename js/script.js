0cf489464964cd443010a205f168055781419dec
Anthony Zheng <Anthony@fopen-dream.space>
2016-05-31 20:48:35 -0400

    new resources and script

--- js/script.js
+++ js/script.js
@@ -19,7 +19,8 @@ var voice = {
 
 // Define the sounds used in the game.
 var sound = {
-    "bird": "crow.mp3"
+    "bird": "crow.mp3",
+    "water": "water.mp3"
 }
 
 // Define the videos used in the game.
@@ -48,9 +49,14 @@ var characters = {
     },
 
     "dad":{
-	"Name": "Dad",
+	"Name": "Richard",
 	"Color": "#5bcaff"
-    }
+    },
+
+    "siri":{
+	"Name": "Siri",
+	"Color": "blue"
+    },
 }
 
 var script = {
@@ -61,28 +67,43 @@ var script = {
 	"I open my eyes to the usual darkness.",
 	"What time is it?",
 	"Where's Father?",
-	"dad Emily, what are you doing?! Hurry up and get ready!",
+	"dad EMILY, WHAT THE FUCK ARE YOU DOIN?! HURRY UP THE HELL UP!",
 	"Father calls me from the kitchen.",
+	
 	"scene bed",
 	"Ah, now I see...it's morning.",
-	"I debate whether to respond to Father, but nothing good has ever come of it.",
+	"I debate whether or not to respond to Father, but nothing good has ever come of it.",
 	"I assume he can tell by the sounds that I'm up.",
 	"A moment passes by as I reflect on my suroundings.",
 	"I live in a rural neighborhood, the sun is shining bright and the birds are humming.",
 	"It's another beautiful day...",
-	"... Did you think I was going to say something like that?",
-	"Ofcourse that's not how things are...",
+	"... Only the mentally ill would say something like that.",
+	"Of course that's not how things are...",
 	"play sound bird",
+	
 	"scene bedroom",
-	"We don't live in that kind of happy-go-lucky world.",
-	"Though, we do actually have birds outside.",
+	"mc We don't live in that kind of happy-go-lucky world.",
+	"mc Though, we do actually have birds outside...",
 	"Getting out of bed, I slowly make my way to the bathroom.",
 	"scene empty",
 	"mc Now... where's the sink?",
 	"I feel around the surroundings in an attempt find where I am.",
 	"Ah...Now I see, here it is.",
+	
 	"scene bath",
-	"Here ends this demo...",
+	"The water turns on and I once again begin my daily ritual.",
+	"play sound water loop",
+	"While I'm washing my face...",
+	"mc HEY SIRI! ...",
+	"siri *Beep Boop*",
+	"I hear Siri's beep letting me know that heard me.",
+	"mc WHAT TIME IS IT?!",
+	"siri At the tone, the time will be 5:41 AM ... beep.",
+	"mc Seems like I'm almost late for school...",
+	"I've just about stopped caring about such things for a while now.",
+	
+	
+	
 
 	{"Choice":{
 	    "Test1":{
